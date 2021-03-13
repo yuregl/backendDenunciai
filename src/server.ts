21f037849c0220b8118043619bcd3e0d09225e16
yuregl <yuregaldino@hotmail.com>
2021-03-13 13:06:08 -0300

    hotfix: Ajuste do eslint e prettier
    Ajuste das config do prettier e tslint pois nao estavam funcionando do
    jeito correto.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 
-const user = 'teste';
-
 const app = express();
 
-app.get('/', (request, response) => {
-	return response.send("Hello World!");
-})
+app.get('/', (request, response) => response.send('Hello World!'));
 
-app.listen(3000, () => console.log("Server is running!"));
+app.listen(3000, () => {
+  console.log('Server is running!');
+});
